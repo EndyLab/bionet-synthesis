aa1872ac08915564145a246439618d8ee29ca463
Anton Jackson-Smith <anton@dyingstar.net>
2017-11-22 14:07:27 -0800

    Do dynamic source plate layout

--- pipeline/ot-build.py
+++ pipeline/ot-build.py
@@ -1,17 +1,50 @@
 from opentrons import robot, containers, instruments
 import pandas as pd
 import argparse
+import sys
+
+# Configuration
+SOURCE_SLOTS = ['D1','D2','D3', 'E1','C2']
 
 # Load our files
 parser = argparse.ArgumentParser(description="Run a DNA build on an Opentrons OT-1 robot.")
-parser.add_argument('-l', '--layout', required=True, help="A CSV file describing the layout of the sourcep plates.")
+parser.add_argument('-l', '--layout', required=False, help="A CSV file describing the layout of the sourcep plates.")
 parser.add_argument('-b', '--build-plan', required=True, help="A CSV file describing the build plan.")
 args = parser.parse_args()
 
-layout = pd.read_csv(args.layout)
-layout = layout.set_index('Position').to_dict()['Name'] # Turn into a location->name dict
-
 plan = pd.read_csv(args.build_plan, usecols=['Gene','Wells'])
+
+if args.layout:
+    # We were given an explicit layout
+    layout = pd.read_csv(args.layout)
+    layout = layout.set_index('Position').to_dict()['Name'] # Turn into a location->name dict
+else:
+    plate_names = plan['Wells'].str.split('-').str[0].unique()
+
+    if len(plate_names) > len(SOURCE_SLOTS):
+        print("Error: This build plan requires too many source plates.")
+        sys.exit(1)
+
+    layout = list(zip(SOURCE_SLOTS[:len(plate_names)], plate_names))
+
+    slots = pd.Series(SOURCE_SLOTS)
+    columns = sorted(slots.str[0].unique())
+    rows = sorted(slots.str[1].unique(), reverse=True)
+
+    layout_table = pd.DataFrame(index=rows, columns=columns)
+    layout_table.fillna("", inplace=True)
+
+    for slot, plate in layout:
+        print(slot[1], slot[0])
+        layout_table.loc[slot[1], slot[0]] = plate
+
+    print("Please arrange the plates in the following configuration:")
+    print(layout_table)
+    print()
+    input("Press enter to continue")
+    sys.exit(0)
+
+    layout = dict(layout)
 
 # Configure the robot
 
@@ -43,14 +76,9 @@ dest_plates = [
     containers.load('96-flat', 'B2')
 ]
 
-source_plates = {
-    layout['C1']: containers.load('96-flat', 'C1'),
-    layout['C2']: containers.load('96-flat', 'C2'),
-    layout['C3']: containers.load('96-flat', 'C3'),
-    layout['D1']: containers.load('96-flat', 'D1'),
-    layout['D2']: containers.load('96-flat', 'D2'),
-    layout['D3']: containers.load('96-flat', 'D3'),
-}
+source_plates = {}
+for slot, plate in layout.items():
+    source_plates[plate] = containers.load('96-flat', slot)
 
 p10 = instruments.Pipette(
     axis='a',
