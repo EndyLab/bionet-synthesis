2cff59be5122afa7f31ad01febe06a8cd4ff1c89
ccmeyer <conarymeyer@Conarys-MacBook-Pro.local>
2018-01-25 20:30:23 -0800

    Updated ot-one scripts to be able to differentiate between the two robots

--- pipeline/build.py
+++ pipeline/build.py
@@ -25,6 +25,15 @@ args = parser.parse_args()
 
 plates = []
 
+# Verify that the correct robot is being used
+if args.run:
+    robot_name = str(os.environ["ROBOT_DEV"][-5:])
+    robot_number = str(input("Run on this robot: {} ? 1-Yes, 2-No ".format(robot_name)))
+    if robot_number == "1":
+        print("Proceeding with run")
+    else:
+        sys.exit("Run . robot.sh while in the /opentrons/robots directory to change the robot")
+
 if args.manual:
     num_reactions = input("How many reactions: ")
     max_plates = input("Number of plates to pull from: ")
@@ -243,7 +252,8 @@ input("Press enter to continue")
 
 # Determine whether to simulate or run the protocol
 if args.run:
-    port = robot.get_serial_ports_list()[0]
+    #port = robot.get_serial_ports_list()[0]
+    port = os.environ["ROBOT_DEV"]
     print("Connecting robot to port {}".format(port))
     robot.connect(port)
 else:
