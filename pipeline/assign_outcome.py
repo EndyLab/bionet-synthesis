6fedd9a4f6427d41f44f1c7f004fb4040c94228d
ccmeyer <conarymeyer@DN0a22b626.SUNet>
2018-02-09 17:45:55 -0800

    Added sequencing data for build004 and modified the status script to show mutations

--- pipeline/assign_outcome.py
+++ pipeline/assign_outcome.py
@@ -11,6 +11,8 @@ import glob
 import re
 import math
 
+import collections
+
 import shutil
 
 import_time = datetime.now()
@@ -23,6 +25,8 @@ DATA_PATH = BASE_PATH + "/data"
 
 BACKBONE_PATH = BASE_PATH + "/sequencing_files/popen_v1-1_backbone.fasta"
 DICTIONARY_PATH = PIPELINE_PATH + "testing/data_testing/10K_CDS.csv"
+
+outcome = []
 
 build_num = "build"+str(input("Which build: ")).zfill(3)
 print("build_num",build_num)
@@ -41,20 +45,51 @@ for index, row in results.iterrows():
         row["Manual"] = row["Outcome"]
         if row["Manual"] == "Perfect":
             row["Manual"] = "Good_sequence"
+
     elif row["Manual"] == "1":
-        row["Manual"] = "Point Mutation"
+        row["Manual"] = "Point_mutation"
     elif row["Manual"] == "Perfect":
         row["Manual"] = "Good_sequence"
+    elif row["Manual"] == "Bad Reads":
+        row["Manual"] = "Bad_reads"
+    elif row["Manual"] == "Unknown Sequence":
+        row["Manual"] = "Unknown_sequence"
+
+    outcome.append(row["Manual"])
 
     with open("{}/{}/{}.json".format(DATA_PATH,row["Gene"],row["Gene"]),"r") as json_file:
         data = json.load(json_file)
     for attempts in data["status"]["build_attempts"]:
-        attempts["build_number"] = "build003"
+        attempts["build_number"] = "build004"
         attempts["build_outcome"] = row["Manual"]
     data["status"]["build_complete"] = row["Manual"]
 
     with open("{}/{}/{}.json".format(DATA_PATH,row["Gene"],row["Gene"]),"w+") as json_file:
         json.dump(data,json_file,indent=2)
+
+print(outcome)
+
+counter = collections.Counter(outcome)
+counter = dict(counter)
+outcomes = list(counter.keys())
+counts = list(counter.values())
+print(counter)
+
+stats = pd.DataFrame({
+    'Outcome' : outcomes,
+    'Count' : counts
+ })
+
+total = stats['Count'].sum()
+
+stats['Percentage'] = (stats['Count'] / total) * 100
+
+stats = stats[['Outcome','Count','Percentage']]
+
+print("total:", stats['Count'].sum())
+
+print(stats)
+
 
 stop = datetime.now()
 runtime = stop - start
