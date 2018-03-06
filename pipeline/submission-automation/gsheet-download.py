930bfb93ad16a8c5421f2d884e86673cb1ef7986
Keoni Gandall <koeng101@gmail.com>
2018-03-06 13:04:03 -0800

    Added legacy gsheet-download and Free Genes csv

---
+++ pipeline/submission-automation/gsheet-download.py
@@ -0,0 +1,18 @@
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+ 
+ 
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('Free Genes-153172d39301.json', scope)
+client = gspread.authorize(creds)
+ 
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Free Genes Project Rolling Submissions (Responses)").sheet1
+ 
+# Extract and print all of the values
+list_of_hashes = sheet.get_all_records()
+print(list_of_hashes)
+
