c60cec807900f73ae8a470f444645f398465cc7b
Conary Meyer <ccmeyer@stanford.edu>
2018-06-15 12:19:04 -0700

    Added a homing step after completion of the run

--- pipeline/sql_build_multiple.py
+++ pipeline/sql_build_multiple.py
@@ -315,6 +315,8 @@ def run_build(session,engine):
 
         p10s.drop_tip()
 
+    robot.home()
+
     commit = int(ot.request_info("Commit changes (1-yes, 2-no): ",type='int'))
     if commit == 1:
         for part in to_build:
